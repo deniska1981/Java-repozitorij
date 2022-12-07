9b3ca830e6dcccbc21999ca5772ff61c7b63a0d7
deniska1981 <dtkalec81@gmail.com>
2022-12-07 22:18:18 +0100

    prva JavaScript zadaca

---
+++ ZADACE/zadaca1/zadaca_js1.js
@@ -0,0 +1,13 @@
+function razbij(e) {
+  e.preventDefault();
+  let text = document.getElementById("text").value;
+  let characterNumber = text.length;
+  let dotNumber = text.split(/[.]/);
+
+  document.getElementById(
+    "characterNumber"
+  ).innerHTML = `Broj znakova: ${characterNumber}`;
+  document.getElementById("sentenceNumber").innerHTML = `Broj rečenica: ${
+    dotNumber.length - 1
+  }`;
+}
